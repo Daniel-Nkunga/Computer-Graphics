be498023457a902767970cc7dabb080487b61f45
Daniel Nkunga <daniel.s.nkunga@gmail.com>
2026-01-26 23:10:36 -0600

    Better commmits

--- Project01/src/main.cpp
+++ Project01/src/main.cpp
@@ -12,7 +12,12 @@ int main()
         {{0.0f, 0.0f, 0.0f}, {1.0f, 0.0f, 0.0f}, {0.5f, 1.0f, 0.0f}},
         {255, 0, 0}
     );
-    cout << "Hello, World! :D" << endl;
+    test.display();
+    test.set({{-0.8f, 0.0f, 0.0f}, {1.0f, 0.0f, -0.8f}, {0.5f, 1.0f, 0.0f}}, {0, 0, 255});
+    test.display();
+    test.set({{0.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 0.0f}});
+    test.display();
+    test.set({255, 255, 255});
     test.display();
     return 0;
 }
