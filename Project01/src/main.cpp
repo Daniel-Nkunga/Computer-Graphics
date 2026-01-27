b2ddfc41b0ca6e4ff3ca3f656710ae14f69e3732
Daniel Nkunga <daniel.s.nkunga@gmail.com>
2026-01-27 15:17:16 -0600

    Step B
    Change coordinates to a vector of coordinates

--- Project01/src/main.cpp
+++ Project01/src/main.cpp
@@ -15,15 +15,20 @@ int main()
 
     for(int i = 0; i < 25; i++) // TODO: change to random number to generate
     {
-        float x = (static_cast<float>(rand()) / static_cast<float>(RAND_MAX)) * 2.0f - 1.0f;
-        float y = (static_cast<float>(rand()) / static_cast<float>(RAND_MAX)) * 2.0f - 1.0f;
-        float z = (static_cast<float>(rand()) / static_cast<float>(RAND_MAX)) * 2.0f - 1.0f;
+        std::vector<std::tuple<float, float, float>> coordinates;
+        for(int j = 0; j < (rand() % 4) + 3; j++)
+        {
+            float x = (static_cast<float>(rand()) / static_cast<float>(RAND_MAX)) * 2.0f - 1.0f;
+            float y = (static_cast<float>(rand()) / static_cast<float>(RAND_MAX)) * 2.0f - 1.0f;
+            float z = (static_cast<float>(rand()) / static_cast<float>(RAND_MAX)) * 2.0f - 1.0f;
+            coordinates.push_back({x, y, z});
+        }
         // cout << x << " " << y << " " << z << endl;
         int color1 = rand() % 255;
         int color2 = rand() % 255;
         int color3 = rand() % 255;
         // cout << color1 << " " << color2 << " " << color3 << endl;
-        polygons.push_back(Polygon({{x, y, z}}, {color1, color2, color3}));
+        polygons.push_back(Polygon(coordinates, {color1, color2, color3}));
         polygons[i].display();
     }
     return 0;
