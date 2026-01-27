3785b8b1fe954f940e6046ecd97436c32538d3fb
Daniel Nkunga <daniel.s.nkunga@gmail.com>
2026-01-27 15:02:54 -0600

    Step B
    Generating random polygons

--- Project01/src/main.cpp
+++ Project01/src/main.cpp
@@ -8,16 +8,23 @@ using namespace std;
 
 int main()
 {
-    Polygon test(
-        {{0.0f, 0.0f, 0.0f}, {1.0f, 0.0f, 0.0f}, {0.5f, 1.0f, 0.0f}},
-        {255, 0, 0}
-    );
-    test.display();
-    test.set({{-0.8f, 0.0f, 0.0f}, {1.0f, 0.0f, -0.8f}, {0.5f, 1.0f, 0.0f}}, {0, 0, 255});
-    test.display();
-    test.set({{0.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 0.0f}});
-    test.display();
-    test.set({255, 255, 255});
-    test.display();
+    // srand(time(0));
+    srand(88);
+
+    std::vector<Polygon> polygons;
+
+    for(int i = 0; i < 25; i++) // TODO: change to random number to generate
+    {
+        float x = (static_cast<float>(rand()) / static_cast<float>(RAND_MAX)) * 2.0f - 1.0f;
+        float y = (static_cast<float>(rand()) / static_cast<float>(RAND_MAX)) * 2.0f - 1.0f;
+        float z = (static_cast<float>(rand()) / static_cast<float>(RAND_MAX)) * 2.0f - 1.0f;
+        // cout << x << " " << y << " " << z << endl;
+        int color1 = rand() % 255;
+        int color2 = rand() % 255;
+        int color3 = rand() % 255;
+        // cout << color1 << " " << color2 << " " << color3 << endl;
+        polygons.push_back(Polygon({{x, y, z}}, {color1, color2, color3}));
+        polygons[i].display();
+    }
     return 0;
 }
