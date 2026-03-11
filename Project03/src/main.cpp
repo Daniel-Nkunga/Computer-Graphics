dfa078793c12bb7a6fd72131b078e844fc90a04a
Daniel Nkunga <daniel.s.nkunga@gmail.com>
2026-03-10 21:18:16 -0500

    Initial commit

---
+++ Project03/src/main.cpp
@@ -0,0 +1,31 @@
+#include <GL/freeglut_std.h>
+#include <GL/gl.h>
+#include <iostream>
+#include <tuple>
+#include <vector>
+#include <cmath>
+#include <stdio.h>
+#include <stdlib.h>
+#ifdef MAC
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+using namespace std;
+
+
+void init()
+{
+    return;
+}
+
+void display()
+{
+    return;
+}
+
+int main(int argc, char *argv[])
+{
+    cout << "Hello terrain!" << endl;
+    return 0;
+}
